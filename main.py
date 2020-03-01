d401aa866bf21fef66ee615374979c375827764c
Paul Martin <paul@blibspace.com>
2020-03-01 18:14:55 +0000

    Add random delay against Insta bot-detection

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ Project:       htb2020-memebot
 File Created:  Sunday, 1st March 2020 1:00:07 am
 Author(s):     Paul Martin
 
-Last Modified: Sunday, 1st March 2020 2:15:54 pm
+Last Modified: Sunday, 1st March 2020 6:12:20 pm
 Modified By:   Paul Martin
 '''
-import schedule, time
+import schedule, time, random
 
 import insta, reddit
 import colour_utils as utils
 
 def auto_post():
+    # Wait another 1s - 20min until post to not get detected as bot
+    delay = random.randrange(65, 60*20)
+    time.sleep(delay)
+
     img_names = reddit.get_newest_memes(1)
     # img_names = ["e1cnz3bhu7g31.jpg", "ghbyqn3xhs531.jpg"]
 
